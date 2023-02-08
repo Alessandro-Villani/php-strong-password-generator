cdf9dbf607e66b542aab3b2bb318d1c5154a3098
Alessandro-Villani <alessvill@libero.it>
2023-02-08 16:27:08 +0100

    added page with generated password

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
 
 include 'partials/characters.php';
+include 'partials/functions.php';
 
 $pw_length = $_GET['pw-lenght'] ?? null;
 
-function random_password($length, $characters)
-{
-    if (!$length || $length < 8 || $length > 20) {
-        return;
-    }
-    $generated_password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $type_index = rand(0, count($characters) - 1);
-        $char_index = rand(0, count($characters[$type_index]) - 1);
-        $generated_password .= $characters[$type_index][$char_index];
-    }
-    return $generated_password;
-};
+if ($pw_length) {
+    session_start();
 
+    $_SESSION['generated_password'] = random_password($pw_length, $characters);
+
+    header('Location: ./pw_page.php');
+}
 
 ?>
 
